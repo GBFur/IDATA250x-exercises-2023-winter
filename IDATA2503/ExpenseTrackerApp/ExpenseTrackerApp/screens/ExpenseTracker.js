adbf9444df504686e25d34d714d2d39de8dd3719
GBFur <zifergames@gmail.com>
2023-09-12 03:32:37 +0200

    refactor(IDATA2503): removed unnecessary styling and prop

--- IDATA2503/ExpenseTrackerApp/ExpenseTrackerApp/screens/ExpenseTracker.js
+++ IDATA2503/ExpenseTrackerApp/ExpenseTrackerApp/screens/ExpenseTracker.js
@@ -3,20 +3,12 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import ExpensesOutput from "../components/ExpensesOutput";
 
-function ExpenseTracker({ navigation }) {
+function ExpenseTracker() {
   return (
     <View>
       <ExpensesOutput />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 8,
-
-    flex: 1,
-  },
-});
 
 export default ExpenseTracker;
