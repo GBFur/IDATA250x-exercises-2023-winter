240b066e2bb2a1616e13915c2e5634c9dfcacb17
GBFur <zifergames@gmail.com>
2023-09-24 22:52:04 +0200

    feat(IDATA2503): meals app drawer

--- IDATA2503/MealsApp/MealsApp/App.js
+++ IDATA2503/MealsApp/MealsApp/App.js
@@ -6,8 +6,20 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
+import FavoriteScreen from "./screens/FavoriteScreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Categories" component={CategoriesScreen}/>
+      <Drawer.Screen name="Favorites" component={FavoriteScreen}/>
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -17,14 +29,15 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
+            name="Drawer"
+            component={DrawerNavigator}
             options={{
               title: "All Categories",
+              headerShown: false,
             }}
           />
           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
-          <Stack.Screen name="MealDetail" component={MealDetailScreen}/>
+          <Stack.Screen name="MealDetail" component={MealDetailScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- IDATA2503/MealsApp/MealsApp/screens/FavoriteScreen.js
+++ IDATA2503/MealsApp/MealsApp/screens/FavoriteScreen.js
@@ -1,3 +1,5 @@
+import { View, Text } from "react-native";
+
 function FavoriteScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
