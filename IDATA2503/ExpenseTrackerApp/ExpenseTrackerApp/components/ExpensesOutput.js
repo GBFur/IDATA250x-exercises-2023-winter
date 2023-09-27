c24653737065c0905cd4e274403b371ecb6362ef
GBFur <zifergames@gmail.com>
2023-09-27 19:21:42 +0200

    fix: made toast refresh if expense is deleted while a current toast is up & refactor

--- IDATA2503/ExpenseTrackerApp/ExpenseTrackerApp/components/ExpensesOutput.js
+++ IDATA2503/ExpenseTrackerApp/ExpenseTrackerApp/components/ExpensesOutput.js
@@ -1,40 +1,35 @@
-import React, { useEffect, useRef } from "react";
-import { useContext, useState } from "react";
-import { StyleSheet, View, Button, Animated } from "react-native";
-import ExpensesChart from "./ExpensesChart";
-import ExpensesList from "./ExpensesList";
+import React, { useContext, useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { useToast } from "react-native-toast-notifications";
 import { ExpensesContext } from "../store/expenses-context";
 import { deleteExpense, fetchExpenses, undoDeleteExpense } from "../util/http";
-import { useToast } from "react-native-toast-notifications";
+import ExpensesChart from "./ExpensesChart";
+import ExpensesList from "./ExpensesList";
 import CustomToast from "./Toast/CustomToast";
 
 function ExpensesOutput({ expenses }) {
   const expensesContext = useContext(ExpensesContext);
   const [hasDeleted, setHasDeleted] = useState(false);
-  const animateButton = useRef(new Animated.Value(0)).current; // Initial value for opacity: 0
-  const [countdown, setCountdown] = useState(5);
-
-  //TODO:
-  //https://www.npmjs.com/package/react-native-toast-notifications
-
-  //TODO:
-  //ID is not updated when undoing delete (there is a new ID)
+  const [deletedExpenseId, setDeletedExpenseId] = useState(null);
+  const toast = useToast();
 
   // fetch expenses from server
   useEffect(() => {
     async function getExpenses() {
-      const expenses = await fetchExpenses();
-      expenses.reverse();
-      expensesContext.setExpenses(expenses);
+      try {
+        const fetchedExpenses = await fetchExpenses();
+        expensesContext.setExpenses(fetchedExpenses.reverse());
+      } catch (error) {
+        console.error("Failed to fetch expenses", error);
+      }
     }
     getExpenses();
   }, []);
 
-  const toast = useToast();
-
   // show toast when expense is deleted
   useEffect(() => {
     if (hasDeleted) {
+      toast.hideAll();
       toast.show(
         <CustomToast message={"undo delete"} onPress={undoDeleteHandler} />,
         {
@@ -44,7 +39,7 @@ function ExpensesOutput({ expenses }) {
         }
       );
     }
-  }, [hasDeleted]);
+  }, [hasDeleted, deletedExpenseId]);
 
   const undoDeleteHandler = () => {
     const lastDeletedExpense = expensesContext.lastDeletedExpense; // get last deleted expense
@@ -53,12 +48,14 @@ function ExpensesOutput({ expenses }) {
     expensesContext.undoDelete(); // add back to context
 
     setHasDeleted(false);
+    setDeletedExpenseId(null);
     toast.hideAll();
   };
 
   const onExpenseDelete = (id) => {
     expensesContext.deleteExpense(id); //delete from context
     deleteExpense(id); //delete from server
+    setDeletedExpenseId(id);
     setHasDeleted(true);
   };
 
@@ -70,20 +67,6 @@ function ExpensesOutput({ expenses }) {
       <View style={styles.listContainer}>
         <ExpensesList expenses={expenses} onExpenseDelete={onExpenseDelete} />
       </View>
-      <Animated.View
-        style={{
-          ...styles.buttonContainer,
-          marginBottom: animateButton.interpolate({
-            inputRange: [0, 1],
-            outputRange: [-50, 20], // animate from -50px to 0px from the bottom
-          }),
-        }}
-      >
-        <Button
-          title={`Undo Delete ${countdown}`}
-          onPress={undoDeleteHandler}
-        />
-      </Animated.View>
     </View>
   );
 }
