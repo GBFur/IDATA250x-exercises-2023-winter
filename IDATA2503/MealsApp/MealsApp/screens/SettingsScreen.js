f860e8b2980f8fb9c29d521fb48396bb2838efe5
GBFur <zifergames@gmail.com>
2023-10-10 22:56:27 +0200

    feat(categories): add pressable to parent to make the entire box pressable

--- IDATA2503/MealsApp/MealsApp/screens/SettingsScreen.js
+++ IDATA2503/MealsApp/MealsApp/screens/SettingsScreen.js
@@ -1,4 +1,4 @@
-import { Box, Divider, Switch, Text } from "@gluestack-ui/themed";
+import { Box, Divider, Switch, Text, Pressable } from "@gluestack-ui/themed";
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { filterActions } from "../store/redux/filter";
@@ -25,71 +25,75 @@ function SettingsScreen() {
 
   return (
     <Box px="$2">
-      <Box
+      <Pressable
         space="md"
         flexDirection="row"
         justifyContent="space-between"
         alignItems="center"
         py="$4"
         px="$4"
+        onPress={handleGlutenFreeToggle}
       >
         <Box>
           <Text size="xl">Gluten-free</Text>
           <Text size="xs">Only include gluten-free meals.</Text>
         </Box>
-        <Switch defaultValue={false} onValueChange={handleGlutenFreeToggle} />
-      </Box>
+        <Switch value={filters.isGlutenFree} onValueChange={handleGlutenFreeToggle}/>
+      </Pressable>
 
       <Divider />
 
-      <Box
+      <Pressable
         space="md"
         flexDirection="row"
         justifyContent="space-between"
         alignItems="center"
         py="$4"
         px="$4"
+        onPress={handleLactoseFreeToggle}
       >
         <Box>
           <Text size="xl">Lactose-free</Text>
           <Text size="xs">Only include lactose-free meals.</Text>
         </Box>
-        <Switch defaultValue={false} onValueChange={handleLactoseFreeToggle} />
-      </Box>
+        <Switch value={filters.isLactoseFree} onValueChange={handleLactoseFreeToggle}/>
+      </Pressable>
 
       <Divider />
 
-      <Box
+      <Pressable
         space="md"
         flexDirection="row"
         justifyContent="space-between"
         alignItems="center"
         py="$4"
         px="$4"
+        onPress={handleVegetarianToggle}
       >
         <Box>
           <Text size="xl">Vegetarian</Text>
           <Text size="xs">Only include vegetarian meals.</Text>
         </Box>
-        <Switch defaultValue={false} onValueChange={handleVegetarianToggle} />
-      </Box>
+        <Switch value={filters.isVegetarian} onValueChange={handleVegetarianToggle}/>
+      </Pressable>
 
       <Divider />
 
-      <Box
+      <Pressable
         space="md"
         flexDirection="row"
         justifyContent="space-between"
         alignItems="center"
         py="$4"
         px="$4"
+        onPress={handleVeganToggle}
       >
         <Box>
           <Text size="xl">Vegan</Text>
           <Text size="xs">Only include vegan meals</Text>
         </Box>
-        <Switch defaultValue={false} onValueChange={handleVeganToggle} />
-      </Box>
+        <Switch value={filters.isVegan} onValueChange={handleVeganToggle}/>
+      </Pressable>
     </Box>
   );
 }
