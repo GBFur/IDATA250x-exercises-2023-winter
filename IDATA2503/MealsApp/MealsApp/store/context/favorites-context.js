00976ffcad932c1dec75b7bb04fa61fff88ebe49
GBFur <zifergames@gmail.com>
2023-10-22 19:02:28 +0200

    comment out react context store as it's not being used

--- IDATA2503/MealsApp/MealsApp/store/context/favorites-context.js
+++ IDATA2503/MealsApp/MealsApp/store/context/favorites-context.js
@@ -1,33 +1,33 @@
-import { createContext, useState } from "react";
+// import { createContext, useState } from "react";
 
-export const FavoritesContext = createContext({
-  ids: [],
-  addFavorite: (id) => {},
-  removeFavorite: (id) => {},
-});
+// export const FavoritesContext = createContext({
+//   ids: [],
+//   addFavorite: (id) => {},
+//   removeFavorite: (id) => {},
+// });
 
-function FavoritesContextProvider({ children }) {
-  const [favoriteMealIds, setFavoriteMealsIds] = useState([]);
+// function FavoritesContextProvider({ children }) {
+//   const [favoriteMealIds, setFavoriteMealsIds] = useState([]);
 
-  function addFavorite(id) {
-    setFavoriteMealsIds((prev) => [...prev, id]);
-  }
+//   function addFavorite(id) {
+//     setFavoriteMealsIds((prev) => [...prev, id]);
+//   }
 
-  function removeFavorite(id) {
-    setFavoriteMealsIds((prev) => prev.filter((mealId) => mealId !== id));
-  }
+//   function removeFavorite(id) {
+//     setFavoriteMealsIds((prev) => prev.filter((mealId) => mealId !== id));
+//   }
 
-  const value = {
-    ids: favoriteMealIds,
-    addFavorite: addFavorite,
-    removeFavorite: removeFavorite,
-  };
+//   const value = {
+//     ids: favoriteMealIds,
+//     addFavorite: addFavorite,
+//     removeFavorite: removeFavorite,
+//   };
 
-  return (
-    <FavoritesContext.Provider value={value}>
-      {children}
-    </FavoritesContext.Provider>
-  );
-}
+//   return (
+//     <FavoritesContext.Provider value={value}>
+//       {children}
+//     </FavoritesContext.Provider>
+//   );
+// }
 
-export default FavoritesContextProvider;
+// export default FavoritesContextProvider;
